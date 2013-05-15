c660258be9d598779c46b53d0261c5cd7899629d
Julian Taylor <jtaylor.debian@googlemail.com>
2013-05-15 23:52:29 +0200

    add benchmark script

---
+++ npbench.py
@@ -0,0 +1,30 @@
+
+import timeit
+
+def time(fun, dtype, size=10000, number=1000):
+    t = timeit.timeit(setup="import numpy as np; d = np.ones((%s,), dtype=%s)" % (size, dtype),
+                      stmt=fun, number=number)
+    n = "%s %s:" % (dtype, fun)
+    print "%s %s" % (n, ("%0.3g" % t).rjust(40 - len(n)))
+
+if __name__ == "__main__":
+    # non ieee compliant version in numpy outperforms vectorized one
+    #time("np.multiply(d, d)", "np.complex64")
+    #time("np.multiply(d, d)", "np.complex128")
+
+    #last won't fit in L3 cache of most cpus
+    for s in [5, 10000, 500000, 1000000]:
+        print '%.1f, %.1f' % (s/1024. * 4, s/1024. * 8)
+        for dt in ["np.float32", "np.float64"]:
+            time("np.max(d)", dt, s)
+            time("np.min(d)", dt, s)
+            time("np.sum(d)", dt, s)
+            time("np.prod(d)", dt, s)
+            time("np.add(1, d)", dt, s)
+            time("np.add(d, 1)", dt, s)
+            time("np.divide(1, d)", dt, s)
+            time("np.divide(d, 1)", dt, s)
+            time("np.divide(d, d)", dt, s)
+            time("np.add(d, d)", dt, s)
+            time("np.multiply(d, d)", dt, s)
+            time("np.sqrt(d)", dt, s)
