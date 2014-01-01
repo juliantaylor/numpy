55d676fdfefd6d4c9a1153c4ef7153a9eb44194a
Julian Taylor <jtaylor.debian@googlemail.com>
2014-01-01 03:43:59 +0100

    cleanup

--- .travis.sh
+++ .travis.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 set -ex
 
-# this can all be replaced with:
-# apt-get install libpython2.7-dev:i386
-# CC="gcc -m32" LDSHARED="gcc -m32 -shared" LDFLAGS="-m32 -shared" linux32 python setup.py build
-# when travis updates to ubuntu 14.04
-
 # setup env
 if [ -r /usr/lib/libeatmydata/libeatmydata.so ]; then
   # much faster package installation
   export LD_PRELOAD=/usr/lib/libeatmydata/libeatmydata.so
 fi
 
-if [ "$USE_CHROOT" != "1" ] && [ "$USE_BENTO" != "1" ]; then
+
+setup_base()
+{
   # We used to use 'setup.py install' here, but that has the terrible
   # behaviour that if a copy of the package is already installed in
   # the install location, then the new copy just gets dropped on top
@@ -22,14 +19,18 @@ if [ "$USE_CHROOT" != "1" ] && [ "$USE_BENTO" != "1" ]; then
   # have been removed from master. (See gh-2765, gh-2768.)  Using 'pip
   # install' also has the advantage that it tests that numpy is 'pip
   # install' compatible, see e.g. gh-2766...
-#  pip install .
-python setup.py build_ext --inplace
-fi
+  pip install .
+}
 
-if [ -n "$USE_CHROOT" ] && [ $# -eq 0 ]; then
+setup_chroot()
+{
+  # this can all be replaced with:
+  # apt-get install libpython2.7-dev:i386
+  # CC="gcc -m32" LDSHARED="gcc -m32 -shared" LDFLAGS="-m32 -shared" linux32 python setup.py build
+  # when travis updates to ubuntu 14.04
+  DIR=$1
   sudo apt-get -qq -y --force-yes install debootstrap eatmydata
-  DIR=/chroot
-  sudo debootstrap --variant=buildd --include=fakeroot,build-essential --arch=i386 --foreign saucy $DIR
+  sudo debootstrap --variant=buildd --include=fakeroot,build-essential --arch=$ARCH --foreign $DIST $DIR
   sudo chroot $DIR ./debootstrap/debootstrap --second-stage
   sudo rsync -a $TRAVIS_BUILD_DIR $DIR/
   echo deb http://archive.ubuntu.com/ubuntu/ saucy main restricted universe multiverse | sudo tee -a $DIR/etc/apt/sources.list
@@ -37,12 +38,13 @@ if [ -n "$USE_CHROOT" ] && [ $# -eq 0 ]; then
   echo deb http://security.ubuntu.com/ubuntu saucy-security  main restricted universe multiverse | sudo tee -a $DIR/etc/apt/sources.list
   sudo chroot $DIR bash -c "apt-get update"
   sudo chroot $DIR bash -c "apt-get install -qq -y --force-yes eatmydata libatlas-dev libatlas-base-dev gfortran python-dev python-nose"
+}
 
-  sudo chroot $DIR bash -c "cd numpy && ./.travis.sh chroot-run"
-elif [ -n "$USE_BENTO" ] && [ $# -eq 0 ]; then
+setup_bento()
+{
   export CI_ROOT=$PWD
   cd ..
-  
+
   # Waf
   wget http://waf.googlecode.com/files/waf-1.7.13.tar.bz2
   tar xjvf waf-1.7.13.tar.bz2
@@ -50,7 +52,7 @@ elif [ -n "$USE_BENTO" ] && [ $# -eq 0 ]; then
   python waf-light
   export WAFDIR=$PWD
   cd ..
-  
+
   # Bento
   wget https://github.com/cournape/Bento/archive/master.zip
   unzip master.zip
@@ -58,15 +60,15 @@ elif [ -n "$USE_BENTO" ] && [ $# -eq 0 ]; then
   python bootstrap.py
   export BENTO_ROOT=$PWD
   cd ..
-  
+
   cd $CI_ROOT
-  
+
   # In-place numpy build
   $BENTO_ROOT/bentomaker build -i -j
-  # Prepend to PYTHONPATH so tests can be run
-  export PYTHONPATH=$PWD:$PYTHONPATH
-  ./.travis.sh test
-else
+}
+
+run_test()
+{
   # We change directories to make sure that python won't find the copy
   # of numpy in the source directory.
   export PYTHONPATH=$PWD:$PYTHONPATH
@@ -77,7 +79,21 @@ else
   python ../tools/test-installed-numpy.py # --mode=full
   # - coverage run --source=$INSTALLDIR --rcfile=../.coveragerc $(which python) ../tools/test-installed-numpy.py
   # - coverage report --rcfile=../.coveragerc --show-missing
-#  python setup.py build_ext --inplace
-#  python -c "import numpy; numpy.test()"
+}
+
+if [ "$USE_CHROOT" != "1" ] && [ "$USE_BENTO" != "1" ]; then
+  setup_base
+  run_test
+elif [ -n "$USE_CHROOT" ] && [ $# -eq 0 ]; then
+  DIR=/chroot
+  setup_chroot $DIR
+  # run again in chroot with this time testing
+  sudo linux32 chroot $DIR bash -c "cd numpy && ./$(basename $0) test"
+elif [ -n "$USE_BENTO" ] && [ $# -eq 0 ]; then
+  setup_bento
+  # run again this time testing
+  $0 test
+else
+  run_test
 fi
 
